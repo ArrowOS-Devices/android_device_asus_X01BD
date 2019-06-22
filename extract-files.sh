45f24ec6ab28c278b12128ec6df9cd4750b4555b
Bauuuuu <abansode@outlook.com>
2019-06-22 21:47:56 +0200

    X01BD: cleanup & rebrand the extractions scripts to arrow

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=asus
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ARROW_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$ARROW_ROOT"/vendor/arrow/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -55,25 +55,25 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "$DEVICE" "$VENDOR" "$ARROW_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
+#extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+BLOB_ROOT="$ARROW_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
-TWRP_QSEECOMD="$BLOB_ROOT"/recovery/root/sbin/qseecomd
-TWRP_GATEKEEPER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.gatekeeper@1.0-service-qti
-TWRP_KEYMASTER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.keymaster@3.0-service-qti
+TWRP_QSEECOMD="$BLOB_ROOT"/recovery/root/sbin/qseecomd	
+TWRP_GATEKEEPER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.gatekeeper@1.0-service-qti	
+TWRP_KEYMASTER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.keymaster@3.0-service-qti	
 
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_GATEKEEPER"
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_KEYMASTER"
+sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"	
+sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_GATEKEEPER"	
+sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_KEYMASTER"	
 
-IMSCMSERVICE="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice.xml
-IMSCMSERVICE_1_1="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice_1_1.xml
+IMSCMSERVICE="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice.xml	
+IMSCMSERVICE_1_1="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice_1_1.xml	
 
-sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE"
-sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE_1_1"
+sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE"	
+sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE_1_1
 
 . "$MY_DIR"/setup-makefiles.sh
