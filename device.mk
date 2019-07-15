9cb88d3fe7193d3403db66eb89ec2976a32d39a1
kubersharma001 <kubersharma001@gmail.com>
2019-07-15 17:30:33 +0530

    X01BD: build vendor.qti.hardware.camera.device@1.0
    Change-Id: I5afc3a62de3ad32cd7a9c74582935e117da2b51e

--- device.mk
+++ device.mk
@@ -130,6 +130,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     libxml2 \
     SnapdragonCamera
 
