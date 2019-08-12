9ebadaa58764f3d5957038ee4436aa4476baac37
Bauuuuu <abansode@outlook.com>
2019-08-12 20:39:40 +0200

    X01BD: disable sepolicy for Q bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -242,8 +242,8 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2019-05-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_VERS := 28.0
 
 # Tap to wake
