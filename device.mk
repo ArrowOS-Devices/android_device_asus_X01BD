bcf460619978c6205d602d9d01bc04123e72d32a
Bauuuuu <abansode@outlook.com>
2019-09-09 19:00:02 +0000

    X01BD: relocate framework RRO

--- device.mk
+++ device.mk
@@ -489,4 +489,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+#RRO Targets
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 $(call inherit-product, vendor/asus/X01BD/X01BD-vendor.mk)
