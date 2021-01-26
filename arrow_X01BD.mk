9d9dc15a712b3e4225c5f23fa7be02b06f371b72
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-01-26 12:54:19 +0530

    X01BD: lineage => arrow
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- arrow_X01BD.mk
+++ arrow_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 ArrowOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,17 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+ARROW_OFFICIAL := true
+DEVICE_MAINTAINER := SonalSingh
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := arrow_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_X01BDA
+PRODUCT_MODEL := Asus Zenfone Max Pro M2
 PRODUCT_MANUFACTURER := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
