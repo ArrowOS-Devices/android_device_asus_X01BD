5fc5dca62172d826757430f8f11c286f576bf6f4
Sonal Singh <sonal.singh.19993@gmail.com>
2021-10-27 00:44:23 +0000

    X01BD: Switch to walleye FP and Desc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- arrow_X01BD.mk
+++ arrow_X01BD.mk
@@ -29,7 +29,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X01BD_1 \
     PRODUCT_NAME=WW_X01BD \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
