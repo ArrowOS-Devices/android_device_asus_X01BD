884e5a5892b0f2ae9ffe24365b090df0be5c1f79
Sonal Singh <sonal.singh.19993@gmail.com>
2022-01-10 11:27:27 +0000

    X01BD: update to redfin's January FP & Desc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- arrow_X01BD.mk
+++ arrow_X01BD.mk
@@ -29,7 +29,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X01BD_1 \
     PRODUCT_NAME=WW_X01BD \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
